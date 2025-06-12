6b7fb0ffb80eaf39d85b1909787dbe8e6bcea562
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-12 11:00:23 +0100

    t

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,16 +1,16 @@
 use std::io;
 
 fn main() {
-    let mut retries = 0;
+    let mut retries = 1;
     let riddle =  "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
     let answer = "The letter e";
 
     loop {
         println!("{}", riddle);
         let mut buffer = String::new(); 
-        io::stdin().read_line(&mut buffer);
+        let _ = io::stdin().read_line(&mut buffer);
         if buffer.trim() == answer {
-            println!("{}", retries);
+            println!("Number of trials: {}", retries);
             break
         } else {
             retries += 1
