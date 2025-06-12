ec65a94fa29c5716d6d076fe6516db23b87ede3c
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-12 10:31:21 +0100

    fix: typo

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    f - 32 / (9 as f64/5 as f64)
+    f - 32.0 / (9 as f64/5 as f64)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    c * (9 as f64/5 as f64) + 32
+    c * (9 as f64/5 as f64) + 32.0
 }
