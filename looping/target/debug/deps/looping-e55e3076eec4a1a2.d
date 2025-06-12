2db0b01e1d30cd7c118ac1692319d3e4d3f980a0
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-12 10:59:09 +0100

    t

--- looping/target/debug/deps/looping-e55e3076eec4a1a2.d
+++
@@ -1,5 +0,0 @@
-/home/hsalihee/Desktop/piscine-rust/looping/target/debug/deps/looping-e55e3076eec4a1a2: src/main.rs
-
-/home/hsalihee/Desktop/piscine-rust/looping/target/debug/deps/looping-e55e3076eec4a1a2.d: src/main.rs
-
-src/main.rs:
--- looping/target/debug/looping.d
+++
@@ -1,1 +0,0 @@
-/home/hsalihee/Desktop/piscine-rust/looping/target/debug/looping: /home/hsalihee/Desktop/piscine-rust/looping/src/main.rs
