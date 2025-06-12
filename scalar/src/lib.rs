056c7bda609018d7a6b65ea715d0ef5c14ef325f
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-12 10:25:07 +0100

    done

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn sum(a: u8, b: u8) -> u8  {
+    a + b
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn diff(a: i16,b: i16) -> i16 {
+    a - b
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn pro(a: i8, b:i8) -> i8 {
+    a*b
+}
+
+pub fn quo(a: f32, b: f32) -> f32 {
+    a/b
+}
+
+pub fn rem(a: f32, b: f32) -> f32 {
+    a%b
 }
