5e7ef7e40a0493a95282d2563b32f5957f247599
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-12 11:23:07 +0100

    0

--- fibonacci2/src/lib.rs
+++ fibonacci2/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0
+    }
     if n <= 2 {
         return 1
     }
--- fibonacci2/src/main.rs
+++ fibonacci2/src/main.rs
@@ -4,21 +4,7 @@ fn main() {
     println!(
         "The element in the position {} in fibonacci series is {}",
         0,
-        fibonacci(2)
+        fibonacci(0)
     );
-    println!(
-        "The element in the position {} in fibonacci series is {}",
-        4,
-        fibonacci(4)
-    );
-    println!(
-        "The element in the position {} in fibonacci series is {}",
-        22,
-        fibonacci(22)
-    );
-    println!(
-        "The element in the position {} in fibonacci series is {}",
-        20,
-        fibonacci(20)
-    );
+    
 }
