e028bd84f6a8fbd83e9ec87fabee84fc70e9612c
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-14 11:29:46 +0100

    f

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,36 @@
+pub fn capitalize_first(input: &str) -> String {
+    input[0..1].to_ascii_uppercase() + &input[1..]
+}
+
+pub fn title_case(input: &str) -> String {
+    let mut space = false;
+    let mut res = String::new();
+    for (i, c) in input.chars().enumerate() {
+        if space || i == 0 {
+            res.push(c.to_ascii_uppercase());
+            space = false
+        } else {
+            res.push(c.to_ascii_lowercase());
+        }
+        if c == ' ' {
+            space = true
+        }
+    }
+    res
+}
+
+pub fn change_case(input: &str) -> String {
+    let mut res = String::new();
+    for c in input.chars() {
+        if c.is_alphabetic() {
+            if c.is_lowercase() {
+                res.push(c.to_ascii_uppercase())
+            } else {
+                res.push(c.to_ascii_lowercase())
+            }
+        } else {
+            res.push(c)
+        }
+    }
+    res
+}
