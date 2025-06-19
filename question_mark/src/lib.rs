7bd9997d0ccb308fd8238b54b411f75b79f6cab8
xHozey <hamzasaliheddine@hotmail.com>
2025-06-19 12:35:05 +0100

    question

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -1,14 +1,14 @@
 pub struct One {
-    first_layer: Option<Two>
+    pub first_layer: Option<Two>
 }
 pub struct Two {
-    second_layer: Option<Three>
+    pub second_layer: Option<Three>
 }
 pub struct Three {
-    third_layer: Option<Four>
+    pub third_layer: Option<Four>
 }
 pub struct Four {
-    fourth_layer: Option<u16>
+    pub fourth_layer: Option<u16>
 }
 
 impl One {
