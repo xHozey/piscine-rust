e18cd04b87b8fd008add95efd3d1c2fab8b4c3f1
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-20 12:11:08 +0100

    vag

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,3 +1,5 @@
+use json::*;
+
 pub struct Food {
     pub name: String,
     pub calories: (String, String),
@@ -13,12 +15,12 @@ pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
     let mut proteins = 0.;
     let mut fats = 0.;
     for food in foods {
-        calcs += food.calories.1[0..food.calories.1.len()-4].parse::<f64>().unwrap_or(0.) * food.nbr_of_portions;
+        calcs += food.calories.1[0..food.calories.1.len()-4].parse::<f64>().unwrap() * food.nbr_of_portions;
         carbs += food.carbs * food.nbr_of_portions;
         proteins += food.proteins * food.nbr_of_portions;
         fats += food.fats * food.nbr_of_portions
     }
-    let mut res = json::JsonValue::new_object();
+    let mut res = JsonValue::new_object();
     res["calcs"] = format!("{:.2}", calcs).into();
     res["carbs"] = format!("{:.2}", carbs).into();
     res["proteins"] = format!("{:.2}",proteins).into();
