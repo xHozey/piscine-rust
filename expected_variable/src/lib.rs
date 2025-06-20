5615e93ef6c62d6a02bbd74aa52be91cfa67b8c6
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-20 15:00:52 +0100

    f

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -7,11 +7,13 @@ pub fn expected_variable(compare: &str, expected: &str) -> Option<String> {
     if !lower_c.is_case(Case::Snake) && !lower_c.is_case(Case::Camel) {
         return None;
     }
-    let cost = edit_distance(&lower_c, &lower_e);
-    let res = expected.len() - cost * 100 / expected.len();
+    let cost: i32 = edit_distance(&lower_c, &lower_e) as i32;
+
+    let res = 100 - (100 * cost as isize) / expected.len() as isize; 
     if res > 50 {
         Some(format!("%{}", res))
     } else {
         None
     }
 }
+
