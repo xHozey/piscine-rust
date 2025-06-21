d5613487808bae89fa6c01fadfd3a19bfcdcbb67
xHozey <hamzasaliheddine@hotmail.com>
2025-06-21 11:01:05 +0100

    g

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -2,7 +2,12 @@ pub fn talking(text: &str) -> &str {
     if text.trim().is_empty() {
         return "Just say something!"
     }
-    let yelling = text.chars().filter(|c| c.is_alphabetic()).all(|c| c.is_uppercase());
+    let filtered_chars: Vec<_> = text.chars().filter(|c| c.is_alphabetic()).collect();
+    let mut yelling = false;
+    if !filtered_chars.is_empty() && filtered_chars.iter().all(|c| c.is_uppercase()) {
+        yelling = true
+    }
+
     let mark = &text[text.len()-1..];
     match mark {
         "!" => {
@@ -17,7 +22,12 @@ pub fn talking(text: &str) -> &str {
             } 
                 return "Sure."
         },
-        _ => return "Interesting"
+        _ => {
+            if yelling {
+                return "There is no need to yell, calm down!"
+            }
+            return "Interesting"
+        }
     }
 
 }
