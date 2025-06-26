2241ee8844bf7fad043994266b150cd4730c98f3
xHozey <hamzasaliheddine@hotmail.com>
2025-06-26 17:16:02 +0100

    t

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,21 @@
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
+        Node { ref_list: ref_list }
+    }
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        self.ref_list.retain(|r| !Rc::ptr_eq(r, &element));
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+   Rc::strong_count(ref_list)
+}
