fa56958fc5ad733f692d38a197f70b3bf49b1429
xHozey <hamzasaliheddine@hotmail.com>
2025-06-28 13:28:32 +0100

    t

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
+}
+
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(&mut self, str_to_append: String) -> Self {
+        self.value.push_str(&str_to_append);
+        self.clone()
+    }
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
+        self.value.push_str(&nb_to_append.to_string());
+        self.clone()
+    }
+
+    fn remove_punctuation_marks(&mut self) -> Self{
+        let mut res = String::new();
+        for c in self.value.chars() {
+            if c != '.' && c != ',' && c != '?' && c != '!' {
+                res.push(c);
+            }
+        }
+        Self { value: res }
+    }
+}
