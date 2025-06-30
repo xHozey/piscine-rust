e0d727843a9eae67dc3b7067645cb9549ec51d54
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-30 16:59:31 +0100

    tt

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -4,7 +4,10 @@ use chrono::*;
 pub fn commits_per_week(data: &json::JsonValue) -> HashMap<String, u32> {
     let mut hashmap = HashMap::new();
     for obj in data.members() {
-        *hashmap.entry(obj["commit"]["author"]["date"].to_string().parse::<DateTime<Utc>>().unwrap().format("%Y-W%W").to_string()).or_insert(1) += 1;
+        let date = obj["commit"]["author"]["date"].to_string().parse::<DateTime<Utc>>().unwrap();
+        let iso_week = date.iso_week();
+        let week_str = format!("{}-W{}", iso_week.year(), iso_week.week());
+        *hashmap.entry(week_str).or_insert(0) += 1;
     }
     hashmap
 }
