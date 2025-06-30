6f27fd445106b8aea7623ca3e3afbcf7cc754741
hsalihee <hamzasaliheddine@hotmail.com>
2025-06-30 15:03:28 +0100

    t

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,33 +1,36 @@
-use std::ops::{Add, Mul};
-use lalgebra_scalar::Scalar;
-#[derive(Debug, PartialEq, Eq,Clone)]
+#[derive(Debug)]
 pub struct Vector<T: Scalar>(pub Vec<T>);
 
+use std::ops::{Add, Mul};
+use lalgebra_scalar::*;
 
-impl<T: Scalar> Add for Vector<T> {
-	type Output = Vec<<T as Add>::Output>;
+impl<T: Scalar<Item = T> + Add<Output = T> + Mul<Output = T>> Add for Vector<T> {
+	type Output = Option<Self>;
 
-	fn add(self, rhs: Self) -> Self::Output {
-		let mut res = vec![];
-		for i in 0..self.0.len() {
-			res.push(self.0[i] + rhs.0[i]);
+	fn add(self, rhs: Vector<T>) -> Self::Output{
+		if self.0.len() != rhs.0.len() {
+			return None
 		}
-		res
+		let mut res = Self::new();
+		for i in 0..self.0.len() {
+			res.0.push(self.0[i] + rhs.0[i])
+		}
+		Some(res)
 	}
 }
 
-impl<T: Scalar<Item = T> + std::ops::AddAssign> Vector<T> where T: Scalar + Add<Output = T> + Mul<Output = T> {
+impl<T: Scalar<Item = T> + Add<Output = T> + Mul<Output = T>> Vector<T> {
 	pub fn new() -> Self {
-		Self(vec![])
+		Self(Vec::new())
 	}
 
 	pub fn dot(&self, other: &Self) -> Option<T> {
 		if self.0.len() != other.0.len() {
 			return None
 		}
-		let mut res = T::zero();
+		let mut res = <T as Scalar>::zero();
 		for i in 0..self.0.len() {
-			res += self.0[i] * other.0[i]
+			res = res + self.0[i] * other.0[i]
 		}
 		Some(res)
 	}
