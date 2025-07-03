96794c61d0cc34e80f3413c215908aab9aae7814
xHozey <hamzasaliheddine@hotmail.com>
2025-07-03 12:57:39 +0100

    t

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl<'a> Person<'a> {
+    pub fn new(name: &str) -> Person {
+        Person { name: name, age: 0 }
+    }
+}
