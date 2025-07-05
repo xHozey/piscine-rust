aefb5cc4cef94f813c86b4f5615d16b09a910f2c
xHozey <hamzasaliheddine@hotmail.com>
2025-07-05 10:08:46 +0100

    hmm

---
+++ closures/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn first_fifty_even_square() -> Vec<i32> {
+    let x = || {
+        let mut res = Vec::new();
+        let mut acc = 4;
+        while res.len() < 50 {
+            if acc % 2 == 0 {
+                res.push(acc);
+                acc += 1;
+            }
+        }
+        return res
+    };
+    x()
+}
