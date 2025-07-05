01b668d0fb2c66de80a956cc1f9819c60855f727
xHozey <hamzasaliheddine@hotmail.com>
2025-07-05 18:12:34 +0100

    t

---
+++ project_motion/src/lib.rs
@@ -0,0 +1,53 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Object {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ThrowObject {
+    pub init_position: Object,
+    pub init_velocity: Object,
+    pub actual_position: Object,
+    pub actual_velocity: Object,
+    pub time: f32,
+}
+
+impl ThrowObject {
+    pub fn new(init_position: Object, init_velocity: Object) -> ThrowObject {
+        Self {
+            init_position: init_position.clone(),
+            init_velocity,
+            actual_position: init_position.clone(),
+            actual_velocity: init_position,
+            time: 1.,
+        }
+    }
+}
+
+impl Iterator for ThrowObject {
+    type Item = Self;
+    fn next(&mut self) -> Option<Self::Item> {
+        
+        self.actual_velocity = Object {
+            x: self.init_position.x,
+            y: ((self.init_velocity.y - 9.8 * self.time) * 100.).round() / 100.,
+        };
+        let x = self.init_position.x + self.init_velocity.x * self.time;
+        let y = self.init_position.y
+            + self.init_velocity.y * self.time
+            - 1./2. * 9.8 * self.time * self.time;
+        self.actual_position = Object { x: x, y: (y * 100.).round() / 100. };
+        self.time += 1.;
+        if self.actual_position.y <= 0. {
+            return None;
+        }
+        Some(Self {
+            init_position: self.init_position.clone(),
+            init_velocity: self.init_velocity.clone(),
+            actual_position: self.actual_position.clone(),
+            actual_velocity: self.actual_velocity.clone(),
+            time: self.time,
+        })
+    }
+}
