e9a2aa77b2d74a2b1248d747448a87e9887908b0
xHozey <hamzasaliheddine@hotmail.com>
2025-07-05 15:14:20 +0100

    t

---
+++ highest/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Self { numbers: &numbers }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().map(|v| *v)
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().map(|v| *v)
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut cl = self.numbers.to_vec();
+        cl.sort();
+        cl[self.numbers.len()-3..].iter().rev().take(3).map(|v| *v).collect()
+    }
+}
