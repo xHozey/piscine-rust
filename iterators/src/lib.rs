52d4beb764abcaf50146024735e51adb372cf0bc
xHozey <hamzasaliheddine@hotmail.com>
2025-07-05 15:54:56 +0100

    t

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -7,20 +7,22 @@ impl Iterator for Collatz {
     type Item = Self;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.v == 1 || self.v == 0 {
-            return None
+        if self.v <= 1 {
+            return None;
         }
+        let old = self.v;
         if self.v % 2 == 0 {
-            self.v /= 2
+            self.v /= 2;
         } else {
-            self.v = 3*self.v+1
+            self.v = 3 * self.v + 1;
         }
-        Some(*self)
+        Some(Collatz { v: old })
     }
 }
 
+
 impl Collatz {
-	pub fn new(n: u64) -> Self {
+    pub fn new(n: u64) -> Self {
         Self { v: n }
     }
 }
